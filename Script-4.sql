7eaf755b0427f90d6e1600400d566afc77a11fbf
konstanin-zubenko <kons.zubenko@gmail.com>
2025-09-04 18:09:41 +0300

    Add files via upload

---
+++ Script-4.sql
@@ -0,0 +1,19 @@
+SELECT CONCAT(s.first_name , " ", s.last_name) AS 'Сотрудник магазина', cm.city AS 'Город нахождения магазина', COUNT(c.customer_id) AS 'Количество покупателей'
+FROM staff AS s
+JOIN address AS a ON a.address_id = s.address_id
+JOIN city AS cm ON cm.city_id = a.city_id
+JOIN store AS st ON st.store_id = s.store_id
+JOIN customer AS c ON c.store_id = s.store_id
+GROUP BY staff_id
+HAVING COUNT(c.customer_id) > 300;
+
+
+SELECT COUNT(length) AS 'Количество фильмов больше средней продолжительности'
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+SELECT DATE_FORMAT(payment_date, '%Y.%m') AS 'Год и месяц c наибольшей суммой платежей', COUNT(rental_id) AS 'Количество аренд за месяц', SUM(amount) AS 'Cуммa платежей' 
+FROM payment
+GROUP BY DATE_FORMAT(payment_date, '%Y.%m')
+ORDER BY SUM(amount) DESC
+LIMIT 1;
