76252e10d463dc1893fa3527d64192d1f6cd987a
konstanin-zubenko <kons.zubenko@gmail.com>
2025-09-04 18:11:33 +0300

    Update Script-4.sql

--- Script-4.sql
+++ Script-4.sql
@@ -1,3 +1,5 @@
+
+--задани 1
 SELECT CONCAT(s.first_name , " ", s.last_name) AS 'Сотрудник магазина', cm.city AS 'Город нахождения магазина', COUNT(c.customer_id) AS 'Количество покупателей'
 FROM staff AS s
 JOIN address AS a ON a.address_id = s.address_id
@@ -7,11 +9,13 @@ JOIN customer AS c ON c.store_id = s.store_id
 GROUP BY staff_id
 HAVING COUNT(c.customer_id) > 300;
 
-
+--задание 2
 SELECT COUNT(length) AS 'Количество фильмов больше средней продолжительности'
 FROM film
 WHERE length > (SELECT AVG(length) FROM film);
 
+
+--задание 3
 SELECT DATE_FORMAT(payment_date, '%Y.%m') AS 'Год и месяц c наибольшей суммой платежей', COUNT(rental_id) AS 'Количество аренд за месяц', SUM(amount) AS 'Cуммa платежей' 
 FROM payment
 GROUP BY DATE_FORMAT(payment_date, '%Y.%m')
